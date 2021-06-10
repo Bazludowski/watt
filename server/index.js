9e225f609d5b640dff891b37e6a510f66cf1ce82
domin <dominikamatuszyk@interia.pl>
2021-06-11 00:06:07 +0200

    usuniecie .env

--- server/index.js
+++ server/index.js
@@ -8,18 +8,18 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 dotenv.config();
-app.use(bodyParser.json({limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-//const CONNECTION_URL = 'mongodb+srv://userrr:passsword@wiiiiiiiiiiiiii.zk58v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb+srv://userrr:passsword@wiiiiiiiiiiiiii.zk58v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology:true})
-.then(() => app.listen(PORT, () => console.log(`SER: ${PORT}`)))
-.catch((error) => console.log(error.message));
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`SER: ${PORT}`)))
+    .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
 
